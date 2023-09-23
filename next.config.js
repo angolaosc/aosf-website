6d267152f11c8021f8ac38ab29791886abe918a3
trupakufi <marioteixeiravarela@gmail.com>
2023-09-23 04:26:16 +0100

    style: improve files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
