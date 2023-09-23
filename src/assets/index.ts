4f1566f4f0d572263ad0eed0a68607290e7f7d25
trupakufi <marioteixeiravarela@gmail.com>
2023-09-23 04:36:05 +0100

    refactor: isolate images and export it into an once file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,29 @@
-import { CHAOSS_AFRICA_LOGO_opt } from "./Platinum/chaossLogo.svg";
-import { backgroundImg } from "./background.png";
+import CHAOSS_AFRICA_LOGO_opt from "./images/Platinum/chaossLogo.svg";
+import backgroundImg from "./images/background.png";
+import Culysoft from "./images/Silver/culysoft.png";
+import GitHub_Logo_White from "./images/In-kind/GitHub_Logo_White.png";
 
-export { CHAOSS_AFRICA_LOGO_opt, backgroundImg };
+import DL_logo_2018_color from "./images/Community/DL_logo_2018_color.png";
+import MozdevzLogo from "./images/Community/Mozdevz logo - White.png";
+import logoWTM from "./images/Community/logoWTM.png";
+import CDALogo from "./images/Community/CDA.jpg";
+import CDA_Girls from "./images/Community/CDA_Girls-removebg-preview.png";
+
+import PedroFracnoImg from "./images/PedroFranco.jpg";
+import WaltDeJesusImg from "./images/walt.jpg";
+import MrAguinaldo from "./images/Mr_Aguinaldo.jpg";
+
+export {
+  CHAOSS_AFRICA_LOGO_opt,
+  backgroundImg,
+  Culysoft,
+  GitHub_Logo_White,
+  DL_logo_2018_color,
+  MozdevzLogo,
+  logoWTM,
+  CDALogo,
+  CDA_Girls,
+  PedroFracnoImg,
+  WaltDeJesusImg,
+  MrAguinaldo,
+};
--- src/utilities/data.ts
+++ src/utilities/data.ts
@@ -1,26 +1,25 @@
 import {
-  FaLinkedin,
-  FaFacebookF,
+  PropsTypeInformation,
+  PropsTypeLogo,
+  PropsTypeMenu,
+} from "@/interfaces";
+import {
   FaDiscord,
-  FaYoutube,
+  FaFacebookF,
+  FaLinkedin,
   FaTelegramPlane,
+  FaYoutube,
 } from "react-icons/fa";
-import { PropsTypeLogo, PropsTypeInformation, PropsTypeMenu } from "@/interfaces";
 
-import ChaossAfricaLogo from "@/assets/Platinum/chaossLogo.svg";
-import Culysoft from "@/assets/Silver/culysoft.png"
-import GitHub_Logo_White from "@/assets/In-kind/GitHub_Logo_White.png"
-
-import DL_logo_2018_color from "@/assets/Community/DL_logo_2018_color.png"
-import MozdevzLogo from "@/assets/Community/Mozdevz logo - White.png"
-import logoWTM from "@/assets/Community/logoWTM.png"
-import CDALogo from "@/assets/Community/CDA.jpg"
-import CDA_Girls from "@/assets/Community/CDA_Girls-removebg-preview.png"
-
-import PedroFracnoImg from "@/assets/PedroFranco.jpg";
-import WaltDeJesusImg from "@/assets/walt.jpg";
-import MrAguinaldo from "@/assets/Mr_Aguinaldo.jpg";
-
+import { CHAOSS_AFRICA_LOGO_opt, Culysoft, GitHub_Logo_White } from "@/assets";
+import {
+  CDALogo,
+  CDA_Girls,
+  DL_logo_2018_color,
+  MozdevzLogo,
+  logoWTM,
+} from "@/assets";
+import { MrAguinaldo, PedroFracnoImg, WaltDeJesusImg } from "@/assets";
 
 export const menu: PropsTypeMenu[] = [
   {
@@ -61,46 +60,95 @@ export const menu: PropsTypeMenu[] = [
   } */
 ];
 
-export const informations: PropsTypeInformation[] =  [
+export const informations: PropsTypeInformation[] = [
   {
     id: 0,
     title: "Comunicação",
-    description: "Toda comunicação e últimas notificações sobre o evento serão anunciadas no nosso servidor do discord e nas nossas redes sociais. Por favor, use estes canais para qualquer informação."
+    description:
+      "Toda comunicação e últimas notificações sobre o evento serão anunciadas no nosso servidor do discord e nas nossas redes sociais. Por favor, use estes canais para qualquer informação.",
   },
   {
     id: 1,
     title: "Formato do evento",
-    description: "A primeira edição do AOSFest 2023 acontecerá online e em 4 dias. Porém, teremos um Hackathon que será organizado pessoalmente. Por favor, note que mera participação no AOSFest não é um passo directo para participar no hackathon. As inscrições para o Hackathon e para o AOSFest são feitas separadamente. Por favor, consulte a agenda do evento."
+    description:
+      "A primeira edição do AOSFest 2023 acontecerá online e em 4 dias. Porém, teremos um Hackathon que será organizado pessoalmente. Por favor, note que mera participação no AOSFest não é um passo directo para participar no hackathon. As inscrições para o Hackathon e para o AOSFest são feitas separadamente. Por favor, consulte a agenda do evento.",
   },
   {
     id: 2,
     title: "O que é um Open-source hackathon?",
-    description: "Um open-source hackathon é uma forma empolgante e produtiva de reunir desenvolvedores, designers e entusiastas de código aberto para trabalhar juntos na criação e aprimoramento de software de código aberto em um ambiente de colaboração e competição amigável. É uma maneira eficaz de contribuir para projetos de código aberto e aprender com outros profissionais da área."
+    description:
+      "Um open-source hackathon é uma forma empolgante e produtiva de reunir desenvolvedores, designers e entusiastas de código aberto para trabalhar juntos na criação e aprimoramento de software de código aberto em um ambiente de colaboração e competição amigável. É uma maneira eficaz de contribuir para projetos de código aberto e aprender com outros profissionais da área.",
   },
   {
     id: 3,
     title: "O que é o hacktoberfest?",
-    description: "O Hacktoberfest é um evento anual de desenvolvimento de software de código aberto patrocinado pela DigitalOcean, GitHub e outras empresas, que incentiva programadores de todo o mundo a contribuir para projetos de código aberto durante o mês de outubro. Os participantes podem ganhar camisetas e outros brindes ao enviar um número mínimo de solicitações de pull requests (PRs) válidas em repositórios públicos hospedados no GitHub, promovendo assim a colaboração na comunidade de código aberto e o desenvolvimento de habilidades de programação."
-  }
-]
-
-
-
-export const sponsors: PropsTypeLogo[] = [
-  { id: 0, title: 'Platinum', src: ChaossAfricaLogo, alt: "ChaossAfricaLogo", className: "ChaossAfricaLogo", href: "https://twitter.com/chaoss_africa"},
-  { id: 1, title: 'Silver', src: Culysoft, alt: "Culysoft", className: 'silver', href: "https://culysoft.com/",},
-  { id: 2, title: 'in-kind sponsors', src: GitHub_Logo_White, alt: "GitHub_Logo_White", className: 'in__kind__sponsors', href: "https://github.com/"},
-]
-
-export const communityLogos = [
-  { id: 3, src: DL_logo_2018_color, alt: "DL_logo_2018_color", href: "https://designliquido.com.br/"},
-  { id: 1, src: MozdevzLogo, alt: "MozdevzLogo", href: "https://www.mozdevz.org/"},
-  {id: 4, src: logoWTM, alt: "logoWTM", className: "logoWTM", href: "https://www.linkedin.com/company/wtm-luanda/"},
-  {id: 5, src: CDALogo, alt: "CDALogo", className: "CDAlogo", href: "https://www.facebook.com/communitydevangola?mibextid=b06tZ0"},
-  {id: 6, src: CDA_Girls, alt: "CDA_Girls", className: "CDA_Girls", href: "https://www.linkedin.com/company/cda-girls/"}
-
+    description:
+      "O Hacktoberfest é um evento anual de desenvolvimento de software de código aberto patrocinado pela DigitalOcean, GitHub e outras empresas, que incentiva programadores de todo o mundo a contribuir para projetos de código aberto durante o mês de outubro. Os participantes podem ganhar camisetas e outros brindes ao enviar um número mínimo de solicitações de pull requests (PRs) válidas em repositórios públicos hospedados no GitHub, promovendo assim a colaboração na comunidade de código aberto e o desenvolvimento de habilidades de programação.",
+  },
 ];
 
+export const sponsors: PropsTypeLogo[] = [
+  {
+    id: 0,
+    title: "Platinum",
+    src: CHAOSS_AFRICA_LOGO_opt,
+    alt: "ChaossAfricaLogo",
+    className: "ChaossAfricaLogo",
+    href: "https://twitter.com/chaoss_africa",
+  },
+  {
+    id: 1,
+    title: "Silver",
+    src: Culysoft,
+    alt: "Culysoft",
+    className: "silver",
+    href: "https://culysoft.com/",
+  },
+  {
+    id: 2,
+    title: "in-kind sponsors",
+    src: GitHub_Logo_White,
+    alt: "GitHub_Logo_White",
+    className: "in__kind__sponsors",
+    href: "https://github.com/",
+  },
+];
+
+export const communityLogos = [
+  {
+    id: 3,
+    src: DL_logo_2018_color,
+    alt: "DL_logo_2018_color",
+    href: "https://designliquido.com.br/",
+  },
+  {
+    id: 1,
+    src: MozdevzLogo,
+    alt: "MozdevzLogo",
+    href: "https://www.mozdevz.org/",
+  },
+  {
+    id: 4,
+    src: logoWTM,
+    alt: "logoWTM",
+    className: "logoWTM",
+    href: "https://www.linkedin.com/company/wtm-luanda/",
+  },
+  {
+    id: 5,
+    src: CDALogo,
+    alt: "CDALogo",
+    className: "CDAlogo",
+    href: "https://www.facebook.com/communitydevangola?mibextid=b06tZ0",
+  },
+  {
+    id: 6,
+    src: CDA_Girls,
+    alt: "CDA_Girls",
+    className: "CDA_Girls",
+    href: "https://www.linkedin.com/company/cda-girls/",
+  },
+];
 
 export const CONTACTS = [
   { id: 0, title: "Contacto", phone: "+244 936 853 728 +244 935 401 862" },
@@ -140,7 +188,6 @@ export const SOCIALMEDIA = [
   },
 ];
 
-
 export const CONTRIBUTORS = [
   {
     id: 0,
@@ -166,8 +213,6 @@ export const CONTRIBUTORS = [
   },
 ];
 
-
-
 export const BUTTONS = [
   {
     id: 0,
@@ -178,6 +223,6 @@ export const BUTTONS = [
     id: 1,
     content: "Inscrever-se para o Hackathon",
     url: "https://unstop.com/o/nRNxQZ4?lb=t91qM2wi",
-    className: 'hackathon'
+    className: "hackathon",
   },
 ];
